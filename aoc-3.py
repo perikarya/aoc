d29b365622dc7f57f11787f3861c62ff16f54906
perikarya <40226185+perikarya@users.noreply.github.com>
2022-12-03 19:00:03 +0800

    Update aoc-3.py

--- aoc-3.py
+++ aoc-3.py
@@ -25,11 +25,25 @@ for priority, letter in enumerate(letters):
 items_in_both = []
 
 for position, items in enumerate(first_compartment):
-    item_in_both = ''.join(
-        set(items).intersection(second_compartment[position]))
+    item_in_both = ''.join(set(items).intersection(second_compartment[position]))
     items_in_both.append(item_in_both)
 
 items_in_both = [priorities[i] for i in items_in_both]
 
 sum_of_priorities = sum(items_in_both)
 print(sum_of_priorities)
+
+# find common item in each set of three elves' rucksacks
+
+sets_of_three = list(zip(*[iter(rucksacks)]*3))
+
+items_in_all = []
+
+for set_of_three in sets_of_three:
+    item_in_all = ''.join(set(set_of_three[0]).intersection(set_of_three[1], set_of_three[2]))
+    items_in_all.append(item_in_all)
+
+items_in_all = [priorities[i] for i in items_in_all]
+
+sum_of_badge_priorities = sum(items_in_all)
+print(sum_of_badge_priorities)
