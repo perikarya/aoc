f8d101bc00a2edf91a7393ab942a393ed361664f
perikarya <40226185+perikarya@users.noreply.github.com>
2022-12-09 14:12:07 +0800

    Update aoc-6.py

--- aoc-6.py
+++ aoc-6.py
@@ -16,3 +16,32 @@ for index, letter in enumerate(datastreams):
         char_to_be_processed = index
         print(index)
         break
+
+# find index for letter following a set of fourteen letters with no repeats
+
+count = 0
+
+for index, letter in enumerate(datastreams):
+    previous_fourteen = datastreams[(index-14):index]
+    count += 1
+    if count > 14 and len(set(previous_fourteen)) == len(previous_fourteen):
+        char_to_be_processed = index
+        print(index)
+        break
+        
+# refactor into one function for both parts
+
+def finder(number_of_chars):
+
+    count = 0
+
+    for index, letter in enumerate(datastreams):
+        previous_chars = datastreams[(index-number_of_chars):index]
+        count += 1
+        if count > number_of_chars and len(set(previous_chars)) == len(previous_chars):
+            char_to_be_processed = index
+            print(index)
+            break
+
+finder(4)
+finder(14)
