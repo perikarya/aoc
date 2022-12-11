f75d153dcbc371b5eb0e63b0dd344ad776bf5cd7
perikarya <40226185+perikarya@users.noreply.github.com>
2022-12-11 13:12:21 +0800

    Update aoc-4.py

--- aoc-4.py
+++ aoc-4.py
@@ -2,15 +2,19 @@
 
 assignments = [assignment.strip() for assignment in open("aoc-4.txt")]
 
-# find number of assignments fully containing the other in the pair
+# find number of assignments fully containing the other pair, or any element of the other pair
 
-count = 0
+contained_within = 0
+any_overlap = 0
 
 for assignment in assignments:
     assignment = assignment.replace(",", "-").split("-")
     first_section = [i for i in range(int(assignment[0]), int(assignment[1]) + 1)]
     second_section = [i for i in range(int(assignment[2]), int(assignment[3]) + 1)]
     if set(first_section).issubset(set(second_section)) or set(second_section).issubset(set(first_section)):
-        count += 1
+        contained_within += 1
+    if len(set(first_section).intersection(set(second_section))) != 0:
+        any_overlap += 1
     
-print(count)
+print(contained_within)
+print(any_overlap)
